faf66d03093cb399e8250dede30c3e73981aef5c
Deepanshu sharma <raghavsharma70170@gmail.com>
2025-07-17 23:58:13 +0530

    datatypes and conversion

---
+++ const_varible.js
@@ -0,0 +1,6 @@
+const name="Deepanshu sharma";
+const acc_id=123;
+let acc_password="Raghav@123";
+console.log("name");
+console.log(acc_id);
+console.table([name,acc_id,acc_password]);``
---
+++ datatype.js
@@ -0,0 +1,7 @@
+let name="Deepanshu";
+let rollno=90;
+let result=undefined;
+let money=null;
+console.log(typeof "money");
+console.log(typeof null);
+console.log(typeof undefined);
